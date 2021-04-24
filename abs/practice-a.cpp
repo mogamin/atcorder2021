f8f85ac77f88e9907f452ac4803b3089b30437a2
mogamin <tmogami@gmail.com>
2021-04-24 19:23:56 +0900

    addew new

---
+++ abs/practice-a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a=0, b=0, c=0;
+  string s="";
+
+  cin >> a >> b >> c >> s;
+  cout << a + b + c << " " << s << endl;
+}
---
+++ apg4b/ex4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int seconds = 365 * 24 * 60 * 60;
+
+  int year = seconds;
+  cout << year << endl;
+  cout << year * 2 << endl;
+  cout << year * 5 << endl;
+  cout << year * 10 << endl;
+
+}

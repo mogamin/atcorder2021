a1faf35183fbd35a2b96bbc9654b7ade1595abf4
mogamin <tmogami@gmail.com>
2021-04-25 09:14:50 +0900

    added new

---
+++ apg4b/ex9.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x=0,a=0,b=0;
+  cin >> x >> a >> b;
+
+  x += 1;
+  cout << x << endl;
+
+  x *= (a + b);
+  cout << x << endl;
+
+  x *= x;
+  cout << x << endl;
+
+  x -= 1;
+  cout << x << endl;
+  
+
+}

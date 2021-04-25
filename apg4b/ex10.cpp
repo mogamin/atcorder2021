6924b473399f93d89c9bb95e493b0f0e80dff36c
mogamin <tmogami@gmail.com>
2021-04-25 10:26:39 +0900

    added new

---
+++ apg4b/ex10.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a=0, b=0;
+  cin >> a >> b;
+
+  cout << "A:";
+  for (int i=0; i<a; i++){
+    cout << "]";
+  }
+  cout << endl;
+  
+  cout << "B:";
+  for (int i=0; i<b; i++){
+    cout << "]";
+  }
+  cout << endl;
+  
+}

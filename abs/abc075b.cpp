f63e8b070268b075348b3d2f2337837947bb0729
mogamin <tmogami@gmail.com>
2021-04-29 13:55:08 +0900

    add new

---
+++ abs/abc075b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int h,w;
+  cin >> h >> w;
+  
+  vector<string> vHW(h);
+
+  for(int i=0; i<h; i++) {
+    string _v;
+    cin >> _v;
+    vHW.push_back(_v);
+  }
+}

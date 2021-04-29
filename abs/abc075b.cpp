64f997e12941d2a1c84b1f883d4ac9a0ac4296ad
mogamin <tmogami@gmail.com>
2021-04-29 14:48:00 +0900

    added new

--- abs/abc075b.cpp
+++ abs/abc075b.cpp
@@ -10,6 +10,72 @@ int main() {
   for(int i=0; i<h; i++) {
     string _v;
     cin >> _v;
-    vHW.push_back(_v);
+    vHW.at(i) = _v;
   }
+
+  for(int i=0; i<h; i++) {
+    string v = vHW.at(i);
+    for(int j=0 ; j<w; j++) {
+      if (v.at(j) == '#') {
+        cout << "#";
+        continue;
+      }
+
+      int sum = 0;
+      int _i, _j;
+      _i = i-1; _j = j-1;
+      cout << _i << ":" << _j << endl;
+      if ((0<_i) && (_i<h) && (0<_j) && (_j<w)) {
+        cout << "in" << endl;
+        if (vHW.at(_i).at(_j) == '#') sum++;
+      }
+      _i = i-1; _j = j;
+      cout << _i << ":" << _j << endl;
+      if ((0<_i) && (_i<h) && (0<_j) && (_j<w)) {
+        cout << "in" << endl;
+        if (vHW.at(_i).at(_j) == '#') sum++;
+      }
+      _i = i-1; _j = j+1;
+      cout << _i << ":" << _j << endl;
+      if ((0<_i) && (_i<h) && (0<_j) && (_j<w)) {
+        cout << "in" << endl;
+        if (vHW.at(_i).at(_j) == '#') sum++;
+      }
+      _i = i; _j = j-1;
+      cout << _i << ":" << _j << endl;
+      if ((0<_i) && (_i<h) && (0<_j) && (_j<w)) {
+        cout << "in" << endl;
+        if (vHW.at(_i).at(_j) == '#') sum++;
+      }
+      _i = i; _j = j+1;
+      cout << _i << ":" << _j << endl;
+      if ((0<_i) && (_i<h) && (0<_j) && (_j<w)) {
+        cout << "in" << endl;
+        if (vHW.at(_i).at(_j) == '#') sum++;
+      }
+      _i = i+1; _j = j-1;
+      cout << _i << ":" << _j << endl;
+      if ((0<_i) && (_i<h) && (0<_j) && (_j<w)) {
+        cout << "in" << endl;
+        if (vHW.at(_i).at(_j) == '#') sum++;
+      }
+      _i = i+1; _j = j;
+      cout << _i << ":" << _j << endl;
+      if ((0<_i) && (_i<h) && (0<_j) && (_j<w)) {
+        cout << "in" << endl;
+        if (vHW.at(_i).at(_j) == '#') sum++;
+      }
+      _i = i+1; _j = j+1;
+      cout << _i << ":" << _j << endl;
+      if ((0<_i) && (_i<h) && (0<_j) && (_j<w)) {
+        cout << "in" << endl;
+        if (vHW.at(_i).at(_j) == '#') sum++;
+      }
+      cout << sum;
+      
+    }
+    cout << endl;
+  }
+  
 }
+
